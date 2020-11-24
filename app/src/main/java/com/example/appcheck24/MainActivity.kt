6fc2e4803105dcca3069a81114b84728e30d2f78
kajawek <kajawek@gmail.com>
2020-11-24 10:21:50 +0100

    initial

--- app/src/main/java/com/example/appcheck24/MainActivity.kt
+++ app/src/main/java/com/example/appcheck24/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        init()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun init() {
+
+
     }
 }
